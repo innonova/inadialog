ccea8ba5a2018272999f51354f688396d532dc92
Ramon Küpfer <ramon.kuepfer@gmail.com>
2023-10-23 11:18:25 +0200

    Create Endpoint Component

--- apps/diagram/src/app/components/diagram/RelationComponent.vue
+++ apps/diagram/src/app/components/diagram/RelationComponent.vue
@@ -1,27 +1,32 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
 import { path } from '../../composables/curve';
 import type { Point } from '../../composables/curve';
 
-import { useMovement } from '../../composables/svg-element';
+import RelationEndpointComponent from './RelationEndpointComponent.vue';
 
 const props = defineProps<{
   from: Point,
   to: Point
 }>();
 
+const start = reactive(props.from);
+const end = reactive(props.to);
+
 const emit = defineEmits<{
-  (event: 'moveend', position: { from: Point, to: Point }): void
-}>()
+  (event: 'moveend', position: { from: Point, to: Point }): void;
+}>();
 
-const refStartPoint = ref<SVGGElement | null>(null);
-const { position: start } = useMovement(refStartPoint, props.from);
-
-const refEndPoint = ref<SVGGElement | null>(null);
-const { position: end } = useMovement(refEndPoint, props.to);
-
-const moveend = () => {
+const moveStart = (position: Point) => {
+  start.x = position.x;
+  start.y = position.y;
+}
+const moveEnd = (position: Point) => {
+  end.x = position.x;
+  end.y = position.y;
+}
+const moved = () => {
   emit('moveend', { from: { x: start.x, y: start.y }, to: { x: end.x, y: end.y }});
 }
 </script>
@@ -29,22 +34,14 @@ const moveend = () => {
 <template>
     <g data-type="curve">
         <path :d="path(start, end)"></path>
-        <g
-            ref="refStartPoint"
-            class="endpoint"
-            :transform="`translate(${start.x} ${start.y})`"
-            @pointerup="moveend">
-            <circle :r="25"></circle>
-            <circle :r="5"></circle>
-        </g>
-        <g
-            ref="refEndPoint"
-            class="endpoint"
-            :transform="`translate(${end.x} ${end.y})`"
-            @pointerup="moveend">
-            <circle :r="25"></circle>
-            <circle :r="5"></circle>
-        </g>
+        <RelationEndpointComponent
+            :position="$props.from"
+            @move="moveStart"
+            @pointerup="moved" />
+        <RelationEndpointComponent
+            :position="$props.to"
+            @move="moveEnd"
+            @pointerup="moved"/>
     </g>
 </template>
 
@@ -53,16 +50,5 @@ g path {
     fill: transparent;
     stroke: #222;
     pointer-events: stroke;
-}
-g circle:first-child {
-    fill: transparent;
-}
-g circle:nth-child(2) {
-    pointer-events: none;
-    fill: transparent;
-}
-g:hover circle:nth-child(2) {
-    fill: black;
-    transition: 0.4s;
 }
 </style>
---
+++ apps/diagram/src/app/components/diagram/RelationEndpointComponent.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import type { Point } from '../../composables/curve';
+
+import { useMovement } from '../../composables/svg-element';
+
+const props = defineProps<{
+  position: Point,
+}>();
+
+const emit = defineEmits<{
+  (event: 'move', position: Point): void
+}>()
+
+const refEndpoint = ref<SVGGElement | null>(null);
+const { position } = useMovement(refEndpoint, props.position);
+
+const move = () => {
+  emit('move', position);
+}
+</script>
+
+<template>
+    <g
+        ref="refEndpoint"
+        class="endpoint"
+        :transform="`translate(${position.x} ${position.y})`"
+        @pointermove="move">
+        <circle :r="25"></circle>
+        <circle :r="5"></circle>
+    </g>
+</template>
+
+<style lang="postcss" scoped>
+g circle:first-child {
+    fill: transparent;
+}
+g circle:nth-child(2) {
+    pointer-events: none;
+    fill: transparent;
+}
+g:hover circle:nth-child(2) {
+    fill: black;
+    transition: 0.4s;
+}
+</style>
